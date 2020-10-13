58413e553d99e8499b5db9c4a5403d03d8747cc8
taishi0109 <taishi0109@gmail.com>
2020-10-13 23:58:47 +0900

    undex1

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,11 @@
 <?php
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::get('/archives/', function() {     
+    return '記事一覧'; 
+});  
+
+Route::get('/archives/{category}/', function($category) {     
+    return $category.'は変態'; 
+});
