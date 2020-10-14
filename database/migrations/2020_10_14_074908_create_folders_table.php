cdcd812e506cbf92faee496ec1a1aaa8d6fba835
taishi0109 <taishi0109@gmail.com>
2020-10-14 17:01:02 +0900

    migretion

--- database/migrations/2020_10_14_074908_create_folders_table.php
+++ database/migrations/2020_10_14_074908_create_folders_table.php
@@ -15,7 +15,7 @@ class CreateFoldersTable extends Migration
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title',20);
+            $table->string('title', 20);
             $table->timestamps();
         });
     }
