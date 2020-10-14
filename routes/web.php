bfba54e2bd1689ee519df0c34b83e0c92f8fb351
taishi0109 <taishi0109@gmail.com>
2020-10-14 13:28:36 +0900

    task

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/{id}/tasks','App\Http\Controllers\TaskController@index')->name('tasks.index');
 
 Route::get('/archives/', function() {     
     return '記事一覧'; 
@@ -8,4 +8,10 @@ Route::get('/archives/', function() {
 
 Route::get('/archives/{category}/', function($category) {     
     return $category.'は変態'; 
+});
+
+Route::get('/', function () {     return view('welcome'); });
+
+Route::get('/info', function () {
+    return 'Hello World';
 });
