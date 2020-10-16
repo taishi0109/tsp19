e3bf9aba7a2ad3f210bdff7f272ba1be85ee790f
taishi0109 <taishi0109@gmail.com>
2020-10-16 20:00:10 +0900

    4-1ok

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -11,9 +12,16 @@ class TaskController extends Controller
     {
         $folders = Folder::all();
 
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+    
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+    
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
