dd62add34d1597a8ef651f356484b9e87bcb3ff4
taishi0109 <taishi0109@gmail.com>
2020-10-18 12:31:17 +0900

    7-last

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateF
 Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
 Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
 Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
 // Route::get('/archives/', function() {     
 //     return '記事一覧'; 
 // });  
