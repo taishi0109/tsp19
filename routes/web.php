7f0401e70be9490c02566b3ed4bbab855358c465
taishi0109 <taishi0109@gmail.com>
2020-10-19 01:32:52 +0900

    8-1

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@c
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
-
+Auth::routes();
 // Route::get('/archives/', function() {     
 //     return '記事一覧'; 
 // });  
@@ -22,3 +22,6 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 //     return 'Hello World';
 
 // });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
